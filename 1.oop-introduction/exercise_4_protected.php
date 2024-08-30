22e58af958540bc712b9f0bcc3592208fac25a3b
AliSepar <alisabah.separ1@gmail.com>
2024-08-30 12:11:26 +0200

    const/public

--- 1.oop-introduction/exercise_4_protected.php
+++ 1.oop-introduction/exercise_4_protected.php
@@ -12,3 +12,92 @@ TODO: Don't call getters in de child class.
 
 USE TYPEHINTING EVERYWHERE!
 */
+
+
+class Beverage
+{
+    protected $color;
+    protected $price;
+    protected $temperature;
+
+    public function __construct($color, $price, $temperature = "cold")
+    {
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = $temperature;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    // Setter for color
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    // Getter for price
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    //getter function for the property/veriable
+    public function getTemperature()
+    {
+        return $this->temperature;
+    }
+
+    public function getInfo()
+    {
+        return "This beverage is " . $this->temperature . " and " . $this->color;
+    }
+}
+
+class Beer extends Beverage
+{
+    private $name;
+    private $alcoholPercentage;
+
+
+    public function __construct($color, $price, $temperature, $name, $alcoholPercentage)
+    {
+        parent::__construct($color, $price, $temperature);
+        $this->name = $name;
+        $this->alcoholPercentage = $alcoholPercentage;
+    }
+
+    public function getAlcoholPercentage()
+    {
+        return $this->alcoholPercentage;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    // Private method beerInfo
+    private function beerInfo()
+    {
+        return "Hi, I'm " . $this->name . " and have an alcohol percentage of " . $this->alcoholPercentage . " and I have a " . $this->color . " color.";
+    }
+
+    // Public method to print beer info
+    public function printBeerInfo()
+    {
+        echo $this->beerInfo();
+    }
+}
+
+
+$Duvel = new Beer('blond', '3.5 euro', 'cold', 'Duvel', '8.5%');
+
+echo $Duvel->getAlcoholPercentage() . "<br>";
+echo $Duvel->getColor() . "<br>";
+echo $Duvel->getInfo() . "<br>";
+$Duvel->setColor('light');
+echo $Duvel->getColor() . "<br>";
+
+$Duvel->printBeerInfo();
--- 1.oop-introduction/exercise_5_public.php
+++ 1.oop-introduction/exercise_5_public.php
@@ -10,3 +10,32 @@ TODO: Change the properties to private.
 TODO: Fix the errors without using getter and setter functions.
 TODO: Change the price to 3.5 euro and print it also on the screen on a new line.
 */
+
+
+class Beverage
+{
+    private  $color;
+    private  $price;
+    private $temperature;
+
+    public function __construct($color, $price, $temperature = "cold")
+    {
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = $temperature;
+    }
+
+    //getter function for the property/veriable
+    // public function getTemperature()
+    // {
+    //     return $this->temperature;
+    // }
+
+    public function getInfo()
+    {
+        return "This beverage is " . $this->temperature . " and " . $this->color . " with a price of " . $this->price . ".";
+    }
+}
+
+$cola = new Beverage('black', '3.5 euro');
+echo $cola->getInfo() . '<br>';
